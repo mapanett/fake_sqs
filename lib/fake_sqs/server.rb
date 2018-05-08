a38811de6c8ac88eb60f5198b97352a10154ae99
Matthew Panetta <matthew.panetta@redbubble.com>
2018-05-08 12:09:17 +1000

    Apply hostname change from feathj/fake_sqs

--- lib/fake_sqs/server.rb
+++ lib/fake_sqs/server.rb
@@ -6,10 +6,11 @@ module FakeSQS
     def initialize(options)
       @host = options.fetch(:host)
       @port = options.fetch(:port)
+      @hostname = options.fetch(:port, nil)
     end
 
     def url_for(queue_id, options = {})
-      host = options[:host] || @host
+      host = options[:host] || @hostname || @host
       port = options[:port] || @port
 
       "http://#{host}:#{port}/#{queue_id}"
